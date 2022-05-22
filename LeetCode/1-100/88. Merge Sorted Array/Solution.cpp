45b40de53d094781e99c151ee116e226995d8a62
Chennupati Bibushan Sai <99083798+bibushan@users.noreply.github.com>
2022-05-22 16:33:36 +0530

    ADD LeetCode 88

---
+++ LeetCode/1-100/88. Merge Sorted Array/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int pointer1 = m - 1;
+        int pointer2 = n - 1;
+        for (int i = m + n - 1; i > -1; i--) {
+            if ((pointer2 > -1) && ((pointer1 < 0) || nums1[pointer1] < nums2[pointer2])) {
+                nums1[i] = nums2[pointer2];
+                pointer2--;
+            } else {
+                nums1[i] = nums1[pointer1];
+                pointer1--;
+            }
+        }
+    }
+};
