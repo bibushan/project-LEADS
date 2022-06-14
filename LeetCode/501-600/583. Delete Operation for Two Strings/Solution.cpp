68ad29dd16dff5e523a8e4faba4498335ad275c0
bibushan <bibushansai@gmail.com>
2022-06-14 13:01:40 +0530

    ADD LeetCode 583, 1143

---
+++ LeetCode/501-600/583. Delete Operation for Two Strings/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  int minDistance(string word1, string word2) {
+    int m = word1.size();
+    int n = word2.size();
+    int cmnsubseq = longestCommonSubsequence(word1, word2, m, n);
+    int solution = m - cmnsubseq + n - cmnsubseq;
+    return solution;
+  }
+
+  int longestCommonSubsequence(string text1, string text2, int n1, int n2) {
+    int dp[n1][n2];
+    dp[0][0] = text1[0] == text2[0] ? 1 : 0;
+    for (int j = 1; j < n2; ++j) {
+      dp[0][j] = text1[0] == text2[j] ? 1 : dp[0][j - 1];
+    }
+    for (int i = 1; i < n1; ++i) {
+      dp[i][0] = text1[i] == text2[0] ? 1 : dp[i - 1][0];
+      for (int j = 1; j < n2; ++j) {
+        dp[i][j] = text1[i] == text2[j] ? dp[i - 1][j - 1] + 1
+                                        : max(dp[i][j - 1], dp[i - 1][j]);
+      }
+    }
+    return dp[n1 - 1][n2 - 1];
+  }
+};
