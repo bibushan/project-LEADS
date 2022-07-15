8da14b586deeedf195194f431e271cad71a1f666
unknown <bibushansai@gmail.com>
2022-07-15 22:46:36 +0530

    ADD LeetCode 110, 111, 226

---
+++ LeetCode/101-200/110. Balanced Binary Tree/Solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(height(root) == -1)
+            return false;
+        return true;
+    }
+    
+    int height(TreeNode *root) {
+        if(root == NULL)
+            return 0;
+        
+        int left = height(root->left);
+        if(left == -1)
+            return -1;
+        
+        int right = height(root->right);
+        if(right == -1)
+            return -1;
+        
+        if(abs(left - right) > 1)
+            return -1;
+        return max(left, right) + 1;
+    }
+};
---
+++ LeetCode/101-200/111. Minimum Depth of Binary Tree/Solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        } else if((root->right == NULL) && (root->left == NULL)){
+            return 1;
+        } else if(root->right == NULL) {
+            return 1 + minDepth(root->left);
+        } else if(root->left == NULL) {
+            return 1 + minDepth(root->right);
+        } else{
+            return 1 + min(minDepth(root->left), minDepth(root->right));
+        }
+    }
+};
